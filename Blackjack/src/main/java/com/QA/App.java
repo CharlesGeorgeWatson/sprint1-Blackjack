827f2ecf1d91412cae09a4ad411095ff0cb3911d
Danica Davies <anama.1010@yahoo.com>
2023-10-20 14:04:58 +0200

    added player hitting and dealer hitting algo

--- Blackjack/src/main/java/com/QA/App.java
+++ Blackjack/src/main/java/com/QA/App.java
@@ -1,12 +1,55 @@
 package com.QA;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        int num1 = randomNumGen(-1, 30);
-        int num2 = randomNumGen(-1, 30);
-        System.out.println("Player 1 " + num1);
-        System.out.println("Player 2 " + num2);
-        System.out.println("Player "+winCondition2Player(num1, num2)+ " wins.");
+        int player1Sum = 0;
+        int dealerSum = 0;
+//        ArrayList<Integer> player1Hand=new ArrayList<Integer>();
+//        ArrayList<Integer> player2Hand=new ArrayList<Integer>();
+        for (int i = 0; i < 2; i++) {
+//            int drawnCard = randomNumGen(2, 11);
+            player1Sum = addCard(player1Sum);
+            dealerSum = addCard(dealerSum);
+        }
+        do {
+            if (hitRequest(player1Sum, dealerSum)) {
+                player1Sum = addCard(player1Sum);
+            } else {
+                break;
+            }
+        } while (!isBust(player1Sum));
+        while (shouldDealerHit(dealerSum)) {
+            dealerSum = addCard(dealerSum);
+            System.out.println("Dealer hits, new total is : " + dealerSum);
+        }
+        System.out.println("Player final score is " + player1Sum);
+        System.out.println("Dealer final score is " + dealerSum);
+        System.out.println("Player " + winCondition2Player(player1Sum, dealerSum) + " wins.");
+    }
+
+    public static boolean shouldDealerHit(int dealerSum) {
+//        return Math.random() < 0.5 ? true : false; //50% chance
+        return dealerSum <= 16;
+    }
+
+    public static boolean hitRequest(int player1Sum, int dealerSum) {
+        System.out.println("Your hand is currently valued at " + player1Sum);
+        System.out.println("The dealers hand is currently " + dealerSum);
+        System.out.println("would you like to hit? -> Y/N");
+        Scanner scanner = new Scanner(System.in);
+        String hitChoice = scanner.nextLine();
+        if (hitChoice.equalsIgnoreCase("Y")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int addCard(int originalSum) {
+        return originalSum + randomNumGen(2, 11);
     }
 
     public static int randomNumGen(int minNum, int maxNum) { //minNum & maxNum inclusive
@@ -15,7 +58,7 @@ public class App {
     }
 
     public static int winCondition2Player(int player1, int player2) {
-        if (invalidHand(player1) || invalidHand(player2)){
+        if (invalidHand(player1) || invalidHand(player2)) {
             System.out.println("Dodgy dealing");
             return 0;
         }
@@ -34,13 +77,14 @@ public class App {
             return (player1 > player2) ? 1 : 2;
         }
     }
+
     public static boolean isBust(int cardInput) {
         int maxValue = 21;
         return cardInput > maxValue;
     }
 
-    public static boolean invalidHand(int cardInput){
-        return cardInput<2; // 2 is minimum allowed score for a 2 card deal
+    public static boolean invalidHand(int cardInput) {
+        return cardInput < 2; // 2 is minimum allowed score for a 2 card deal
     }
 
 }
