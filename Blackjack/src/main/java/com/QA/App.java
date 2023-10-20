06c09232c3870f17eaeff2900265adb7d3fe3af2
Danica Davies <anama.1010@yahoo.com>
2023-10-20 12:03:57 +0200

    restyled and removed isBust

--- Blackjack/src/main/java/com/QA/App.java
+++ Blackjack/src/main/java/com/QA/App.java
@@ -1,48 +1,41 @@
 package com.QA;
+
 import java.util.Random;
-public class App 
-{
-    public static void main( String[] args ) {
-        int num1=randomNumGen(1,30);
-        int num2=randomNumGen(1,30);
+
+public class App {
+    public static void main(String[] args) {
+        int num1 = randomNumGen(1, 30);
+        int num2 = randomNumGen(1, 30);
         System.out.println("Player 1 " + num1);
         System.out.println("Player 2 " + num2);
         System.out.println(winCondition2Player(num1, num2));
     }
 
-    public static int randomNumGen(int minNum, int maxNum){ //minNum & maxNum inclusive
+    public static int randomNumGen(int minNum, int maxNum) { //minNum & maxNum inclusive
         maxNum++;
-        return (int)(minNum + (Math.random() * (maxNum-minNum)));
-    }
-    
-    public static int winCondition2Player(int player1, int player2){
-        if(isBust(player1)&& isBust(player2)){
-            return 0;
-        }else if(isBust(player1)){
-            return 2;
-        }else if(isBust(player2)){
-            return 1;
-        }else if(player1 == player2){
-            return 0; //draw
-        }else{
-            return (player1>player2) ? 1 : 2;
-        }
+        return (int) (minNum + (Math.random() * (maxNum - minNum)));
     }
 
-    public static boolean isNotBust(int cardInput){
-        int maxValue = 21; 
-        if(cardInput>maxValue){
-            return false;
+    public static int winCondition2Player(int player1, int player2) {
+        if (isBust(player1) && isBust(player2)) {
+            return 0;
+        } else if (isBust(player1)) {
+            return 2;
+        } else if (isBust(player2)) {
+            return 1;
+        } else if (player1 == player2) {
+            return 0; //draw
+        } else {
+            return (player1 > player2) ? 1 : 2;
         }
-        return true;
     }
-    public static boolean isBust(int cardInput){
-    int maxValue = 21;
-    if(cardInput>maxValue){
-        return true;
+    public static boolean isBust(int cardInput) {
+        int maxValue = 21;
+        if (cardInput > maxValue) {
+            return true;
+        }
+        return false;
     }
-    return false;
-}
 
 }
 
