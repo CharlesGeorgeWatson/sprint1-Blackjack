92599fa5f01280047f293585c06f89c9adbfde67
Charles Watson <charlie.g.watson@gmail.com>
2023-10-20 12:19:27 +0100

    Added win messages and inverted isValid hand method

--- Blackjack/src/main/java/com/QA/App.java
+++ Blackjack/src/main/java/com/QA/App.java
@@ -1,14 +1,12 @@
 package com.QA;
 
-import java.util.Random;
-
 public class App {
     public static void main(String[] args) {
-        int num1 = randomNumGen(1, 30);
-        int num2 = randomNumGen(1, 30);
+        int num1 = randomNumGen(-1, 30);
+        int num2 = randomNumGen(-1, 30);
         System.out.println("Player 1 " + num1);
         System.out.println("Player 2 " + num2);
-        System.out.println(winCondition2Player(num1, num2));
+        System.out.println("Player "+winCondition2Player(num1, num2)+ " wins.");
     }
 
     public static int randomNumGen(int minNum, int maxNum) { //minNum & maxNum inclusive
@@ -17,13 +15,20 @@ public class App {
     }
 
     public static int winCondition2Player(int player1, int player2) {
+        if (invalidHand(player1) || invalidHand(player2)){
+            System.out.println("Dodgy dealing");
+            return 0;
+        }
+
         if (isBust(player1) && isBust(player2)) {
+            System.out.println("Both players bust");
             return 0;
         } else if (isBust(player1)) {
             return 2;
         } else if (isBust(player2)) {
             return 1;
         } else if (player1 == player2) {
+            System.out.println("Draw");
             return 0; //draw
         } else {
             return (player1 > player2) ? 1 : 2;
@@ -31,10 +36,11 @@ public class App {
     }
     public static boolean isBust(int cardInput) {
         int maxValue = 21;
-        if (cardInput > maxValue) {
-            return true;
-        }
-        return false;
+        return cardInput > maxValue;
+    }
+
+    public static boolean invalidHand(int cardInput){
+        return cardInput<2; // 2 is minimum allowed score for a 2 card deal
     }
 
 }
